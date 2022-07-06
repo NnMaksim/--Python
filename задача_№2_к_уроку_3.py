d64d117508815764fc570a1eded5b2ac3d93a97c
MaksimShhenin <maksimshhenin@gmail.com>
2022-07-06 22:03:02 +0300

    решение

---
+++ задача_№2_к_уроку_3.py
@@ -0,0 +1,3 @@
+def pers_info(name, surname, birthday, city, email, phone):
+    print(f'name - {name}; surname - {surname};  birthday - {birthday}; city - {city}, email - {email}; phone - {phone}')
+pers_info(name='Maksim', surname='Lenin', birthday='02.07.1982', city='SPb', email='sphinxNe@.ru', phone='+7999-999-99-99')
